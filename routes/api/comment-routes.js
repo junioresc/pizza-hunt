fe1a5c612a321b4e5bfb0288587a92c770930c6d
Junioresc <junioresc1092@gmail.com>
2020-10-01 16:56:49 -0400

    added functionality to the singel pizza page and let users reply to comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,8 +1,17 @@
 const router = require(`express`).Router();
-const { addComment, removeComment } = require(`../../controllers/comment-controller`);
+const { addComment, removeComment, addReply, removeReply } = require(`../../controllers/comment-controller`);
 
-router.route(`/:pizzaId`).post(addComment);
+router
+    .route(`/:pizzaId`)
+    .post(addComment);
 
-router.route(`/:pizzaId/:commentId`).delete(removeComment);
+router
+    .route(`/:pizzaId/:commentId`)
+    .put(addReply)
+    .delete(removeComment);
 
+router
+    .route(`/:pizzaId/:commentId/:replyId`)
+    .delete(removeReply);
+    
 module.exports = router;
